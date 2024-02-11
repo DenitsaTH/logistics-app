78badfcba05951ecb1d82ca652b96a2b36bf0a2f
DenitsaTH <denizza.cristova@gmail.com>
2024-02-11 20:33:32 +0200

    Adding more validation helpers

--- core/validation_helpers.py
+++ core/validation_helpers.py
@@ -1,3 +1,23 @@
-def check_params_count(min_expected_count, actual_params, cmd_name):
-    if min_expected_count > actual_params:
-        raise ValueError(f'At least {min_expected_count} params expected for {cmd_name}')
+def ensure_valid_params_count(min_expected_count=0, actual_params=0, max_expected=0):
+    if min_expected_count == max_expected and min_expected_count != actual_params:
+        return False
+    if min_expected_count != 0 and min_expected_count <= actual_params:
+        return True
+    if max_expected != 0 and max_expected >= actual_params:
+        return True
+    return False
+
+
+def check_if_valid_stop(stops):
+    for stop in stops:
+        if stop.lower() not in ['syd', 'mel', 'adl', 'asp', 'bri', 'dar', 'per']:
+            return False
+    return True    
+    
+
+def parse_to_integer(s):
+    try:
+        return int(s)
+    except ValueError:
+        return
+    
